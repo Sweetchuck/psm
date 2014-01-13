1b6e9ab0e91493cd99cb75647d6fd3219b7c0edb
Dávid Andor <david-andor@kozpontiagy.hu>
2014-01-13 23:16:45 +0100

    API documentation.

--- psm.api.php
+++ psm.api.php
@@ -2,9 +2,40 @@
 
 /**
  * @file
- * Documentation missing.
+ * Personal Service Manager API documentation.
  */
 
+/**
+ * Provide information about the services.
+ *
+ * @see psm_info_service()
+ * @see _psm_info_service_defaults()
+ */
 function hook_psm_service_info() {
-  // @todo.
+  $info = array();
+
+  $info['my_service_1'] = array(
+    // You do not have to specify this, because this will be filled based on the
+    // top level array key.
+    'name' => 'my_service',
+    // Human readable name of this service.
+    'label' => 'My Service',
+    // Long description about the service.
+    'description' => dt('The Apache HTTP Server Project is a collaborative software development effort aimed at creating a robust, commercial-grade, featureful, and freely-available source code implementation of an HTTP (Web) server.'),
+    // Useful links.
+    'links' => array(
+      'home' => 'http://httpd.apache.org',
+    ),
+    // Name of the handler class.
+    // Can be a string or an array of strings.
+    'class' => '\namespace\to\myclass',
+    // In this case the instance definition have to refers to one of the api ID.
+    'class' => array(
+      // Key is custom, the value is a class name.
+      'my_api_id_1' => '\namespace\to\myclass1',
+      'my_api_id_2' => '\namespace\to\myclass2',
+    ),
+  );
+
+  return $info;
 }

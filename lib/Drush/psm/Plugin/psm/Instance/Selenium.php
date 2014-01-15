d59d5ebf557a02f5f536f84660f04035c61203b1
Dávid Andor <david-andor@kozpontiagy.hu>
2014-01-15 19:13:45 +0100

    Get selenium version basedon file name.

--- lib/Drush/psm/Plugin/psm/Instance/Selenium.php
+++ lib/Drush/psm/Plugin/psm/Instance/Selenium.php
@@ -10,6 +10,19 @@ use Drush\psm\InstanceBasePid;
 
 class Selenium extends InstanceBasePid {
 
+  public function version() {
+    if ($this->versionNumber === NULL) {
+      $this->versionNumber = '';
+      $executable = realpath($this->getInfoEntry('jar'));
+      $matches = array();
+      if (preg_match('/(?P<version>\d.*?)\.jar$/', pathinfo($executable, PATHINFO_BASENAME), $matches)) {
+        $this->versionNumber = $matches['version'];
+      }
+    }
+
+    return $this->versionNumber;
+  }
+
   /**
    * Get the command to start the instance.
    *

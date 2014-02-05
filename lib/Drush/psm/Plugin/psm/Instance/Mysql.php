1343ba4e44eb822a00827675c457efd4b7602491
Dávid Andor <david-andor@kozpontiagy.hu>
2014-02-05 23:06:08 +0100

    Issue #4 Implement the MySQL support.
    Basic support. Get the version number.

--- lib/Drush/psm/Plugin/psm/Instance/Mysql.php
+++ lib/Drush/psm/Plugin/psm/Instance/Mysql.php
@@ -2,4 +2,26 @@
 
 /**
  * @file
+ * Home of the MySQL instance handler class.
  */
+
+namespace Drush\psm\Plugin\psm\Instance;
+
+use Drush\psm\InstanceBase;
+
+/**
+ * Mysql instance handler class.
+ */
+class Mysql extends InstanceBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $versionOption = '--help';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $versionPattern = '/(?<=Ver )(?P<version>\d[^\s]{0,})/';
+
+}

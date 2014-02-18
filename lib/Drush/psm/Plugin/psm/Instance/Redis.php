83a20ba07b2fad73a084de0fd62f20d3d69d80ec
Dávid Andor <david-andor@kozpontiagy.hu>
2014-02-18 19:38:31 +0100

    Issue #6 Implement the Redis support.

--- lib/Drush/psm/Plugin/psm/Instance/Redis.php
+++ lib/Drush/psm/Plugin/psm/Instance/Redis.php
@@ -2,4 +2,42 @@
 
 /**
  * @file
+ * Home of the Redis instance manager class.
  */
+
+namespace Drush\psm\Plugin\psm\Instance;
+
+use Drush\psm\InstanceBase;
+
+/**
+ * Redis instance manager.
+ */
+class Redis extends InstanceBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $executableNames = array(
+    'redis-server',
+  );
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getStartCommand() {
+    $command = parent::getStartCommand();
+
+    $command->optionProperties['key_value_separator'] = ' ';
+
+    $command->executable .= ' %s';
+    $command->arguments[] = $this->getInfoEntry('config_file');
+
+    $options = $this->getInfoEntry('executable_options', FALSE, array());
+    if ($options) {
+      $command->addOptions($options);
+    }
+
+    return $command;
+  }
+
+}

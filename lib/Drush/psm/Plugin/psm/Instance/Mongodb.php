e6ef1d0d480ec03e3b6d684cbb186ba9b55bb68e
Dávid Andor <david-andor@kozpontiagy.hu>
2014-02-19 10:24:36 +0100

    Issue #3 Implement the MongoDB support.

--- lib/Drush/psm/Plugin/psm/Instance/Mongodb.php
+++ lib/Drush/psm/Plugin/psm/Instance/Mongodb.php
@@ -2,4 +2,67 @@
 
 /**
  * @file
+ * Home of the MongoDB instance manager class.
  */
+
+namespace Drush\psm\Plugin\psm\Instance;
+
+use Drush\psm\Command;
+use Drush\psm\InstanceBase;
+
+/**
+ * Redis instance manager.
+ */
+class Mongodb extends InstanceBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $executableNames = array(
+    'mongod',
+  );
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $versionOption = '--version';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getStartCommand() {
+    $command = parent::getStartCommand();
+
+    $command->optionProperties['key_value_separator'] = ' ';
+    $options = $this->getInfoEntry('executable_options', FALSE, array());
+    if ($options) {
+      $command->addOptions($options);
+    }
+
+    return $command;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getStopCommand() {
+    $command = new Command();
+
+    $command->executable = $this->getInfoEntry('executable');
+    $command->optionProperties['key_value_separator'] = ' ';
+
+    $options = array();
+
+    $all_options = $this->getInfoEntry('executable_options', FALSE, array());
+    if (!empty($all_options['--config'])) {
+      $options['--config'] = $all_options['--config'];
+    }
+
+    $options['--shutdown'] = TRUE;
+
+    $command->addOptions($options);
+
+    return $command;
+  }
+
+}
